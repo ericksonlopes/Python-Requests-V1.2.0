1e1f10ca1083fe8db3667dae6ff8a7c15e7e6843
Erickson-lopes-dev <ofc.erickson@gmail.com>
2021-01-07 10:40:16 -0300

    07 bug fix

--- quickstart/07_POST_arquivo_Multipart.py
+++ quickstart/07_POST_arquivo_Multipart.py
@@ -2,6 +2,12 @@ import requests
 
 url = 'http://httpbin.org/post'
 
+# Você pode definir o nome do arquivo explicitamente:
+# files = {'file': ('report.xls', open('report.xls', 'rb'))}
+
+# Se você quiser, você pode enviar strings para serem recebidas como arquivos:
+# files = {'file': ('report.csv', 'some,data,to,send\nanother,row,to,send\n')}
+
 files = {'file': open('07_text.txt', 'rb')}
 
 r = requests.post(url, files=files)
